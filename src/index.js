23163422ef4e6014afe963730ffa7b5aae47e305
Bing Steup <bingsteup@gmail.com>
2019-04-26 15:43:59 +0200

    Add prettier Add eslint

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
-// Find returns an value
-export const mapValueResponse = (keys, field, values, { emptyValue = null } = {}) => (
-  mapper('find', keys, field, values)
-    .map(item => item == undefined ? emptyValue : item)
-)
-
-// Filter returns an array
-export const mapArrayResponse = (...args) => mapper('filter', ...args, [])
+// Get the attributes from nested values
+const get = (p, o) => p.reduce((xs, x) => (xs && xs[x] ? xs[x] : null), o);
 
 // Share logic between mapArray and mapValue
 const mapper = (funcName, keys, field, values) => {
   if (!Array.isArray(keys)) {
-    throw 'Keys must be an array'
+    throw new Error("Keys must be an array");
   }
 
-  if (typeof field !== 'string') {
-    throw 'Field must be a string'
+  if (typeof field !== "string") {
+    throw new Error("Field must be a string");
   }
 
-  const splittedField = field.split('.')
-  return keys.map(key => values[funcName](item => key == get(splittedField, item)))
-}
+  const splittedField = field.split(".");
+  return keys.map(key =>
+    values[funcName](item => key === get(splittedField, item))
+  );
+};
 
-// Get the attributes from nested values
-const get = (p, o) => (
-  p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o)
-)
+// Find returns an value
+export const mapValueResponse = (
+  keys,
+  field,
+  values,
+  { emptyValue = null } = {}
+) =>
+  mapper("find", keys, field, values).map(item =>
+    item === undefined ? emptyValue : item
+  );
+
+// Filter returns an array
+export const mapArrayResponse = (...args) => mapper("filter", ...args, []);
+
+// Value to value
+export const mapValueToValueResponse = mapValueResponse;
+
+// Value to array
+export const mapValueToArrayResponse = mapArrayResponse;
+
+// Array to Value
+export const mapArrayToValueResponse = (arrKeys, ...args) =>
+  arrKeys.map(keys => mapValueResponse(keys, ...args));
+
+// Array to Array
+export const mapArrayToArrayResponse = (arrKeys, ...args) =>
+  arrKeys.map(keys => mapArrayResponse(keys, ...args));
--- src/index.test.js
+++ src/index.test.js
@@ -1,181 +1,195 @@
-import rewire from 'rewire'
-import { mapValueResponse, mapArrayResponse, __RewireAPI__ as Internal } from './'
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line no-unused-vars
+import rewire from "rewire";
+import {
+  mapValueResponse,
+  mapArrayResponse,
+  // eslint-disable-next-line import/named
+  __RewireAPI__ as Internal
+} from ".";
 
-describe('#mapValueResponse', () => {
-  let keys, field, values
-
-  beforeEach(() => {
-    keys = [1, 2]
-    field = 'id'
-    values = [
-      { id: 1, value: 'Chuck' },
-      { id: 2, value: 'Norris' },
-      { id: null, value: 'James Bond' },
-    ]
-  })
-
-  it('aligns the correct value to the key', () => {
-    const expectedResponse = [
-      { id: 1, value: 'Chuck' },
-      { id: 2, value: 'Norris' },
-    ]
-
-    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse)
-  })
-
-  it('works on any key', () => {
-    keys = ['Norris', 'Chuck']
-    field = 'value'
-
-    const expectedResponse = [
-      { id: 2, value: 'Norris' },
-      { id: 1, value: 'Chuck' },
-    ]
-
-    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse)
-  })
-
-  it('works on nested keys', () => {
-    keys = ['Norris', 'Chuck']
-    field = 'value.name'
-    values = [
-      { id: 1, value: { name: 'Chuck' } },
-      { id: 2, value: { name: 'Norris' } },
-    ]
-
-    const expectedResponse = [
-      { id: 2, value: { name: 'Norris' } },
-      { id: 1, value: { name: 'Chuck' } },
-    ]
-
-    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse)
-  })
-
-  it('adds null when a key is not found', () => {
-    keys = [2, 1, null, 3]
-
-    const expectedResponse = [
-      { id: 2, value: 'Norris' },
-      { id: 1, value: 'Chuck' },
-      { id: null, value: 'James Bond' },
-      null,
-    ]
-
-    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse)
-  })
-
-  it('adds custom value when a key is not found and custom value is specified', () => {
-    keys = [2, 1, 3]
-
-    const expectedResponse = [
-      { id: 2, value: 'Norris' },
-      { id: 1, value: 'Chuck' },
-      false,
-    ]
-
-    expect(mapValueResponse(keys, field, values, { emptyValue: false })).toEqual(expectedResponse)
-  })
-})
-
-describe('#mapArrayResponse', () => {
-  let keys, field, values
+describe("#mapValueResponse", () => {
+  let keys;
+  let field;
+  let values;
 
   beforeEach(() => {
-    keys = [1, 2, 3]
-    field = 'externalId'
+    keys = [1, 2];
+    field = "id";
     values = [
-      { externalId: 1, value: 'Chuck' },
-      { externalId: 2, value: 'Norris' },
-      { externalId: 3, value: 'Bruce' },
-      { externalId: 3, value: 'Lee' },
-      { externalId: null, value: 'James Bond' },
-    ]
-  })
+      { id: 1, value: "Chuck" },
+      { id: 2, value: "Norris" },
+      { id: null, value: "James Bond" }
+    ];
+  });
 
-  it('aligns the correct value to the key', () => {
+  it("aligns the correct value to the key", () => {
     const expectedResponse = [
-      [{ externalId: 1, value: 'Chuck' }],
-      [{ externalId: 2, value: 'Norris' }],
-      [
-        { externalId: 3, value: 'Bruce' },
-        { externalId: 3, value: 'Lee' },
-      ],
-    ]
+      { id: 1, value: "Chuck" },
+      { id: 2, value: "Norris" }
+    ];
 
-    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse)
-  })
+    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse);
+  });
 
-  it('works on any key', () => {
-    const keys = ['Chuck', 'Norris']
-    const field = 'value'
+  it("works on any key", () => {
+    keys = ["Norris", "Chuck"];
+    field = "value";
 
     const expectedResponse = [
-      [{ externalId: 1, value: 'Chuck' }],
-      [{ externalId: 2, value: 'Norris' }],
-    ]
+      { id: 2, value: "Norris" },
+      { id: 1, value: "Chuck" }
+    ];
 
-    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse)
-  })
+    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse);
+  });
 
-  it('works on nested keys', () => {
-    const keys = ['Chuck', 'Norris']
-    const field = 'value.name'
-    const values = [
-      { id: 1, value: { name: 'Norris' } },
-      { id: 2, value: { name: 'Chuck' } },
-    ]
-
-    const expectedResponse = [
-      [{ id: 2, value: { name: 'Chuck' } }],
-      [{ id: 1, value: { name: 'Norris' } }],
-    ]
-  })
-
-  it('adds [] when a key is not found', () => {
-    const keys = [1, 2, 4]
+  it("works on nested keys", () => {
+    keys = ["Norris", "Chuck"];
+    field = "value.name";
+    values = [
+      { id: 1, value: { name: "Chuck" } },
+      { id: 2, value: { name: "Norris" } }
+    ];
 
     const expectedResponse = [
-      [{ externalId: 1, value: 'Chuck' }],
-      [{ externalId: 2, value: 'Norris' }],
-      [],
-    ]
+      { id: 2, value: { name: "Norris" } },
+      { id: 1, value: { name: "Chuck" } }
+    ];
 
-    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse)
-  })
-})
+    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse);
+  });
 
-describe('#mapper', () => {
-  let mapper, keys, field, values
+  it("adds null when a key is not found", () => {
+    keys = [2, 1, null, 3];
+
+    const expectedResponse = [
+      { id: 2, value: "Norris" },
+      { id: 1, value: "Chuck" },
+      { id: null, value: "James Bond" },
+      null
+    ];
+
+    expect(mapValueResponse(keys, field, values)).toEqual(expectedResponse);
+  });
+
+  it("adds custom value when a key is not found and custom value is specified", () => {
+    keys = [2, 1, 3];
+
+    const expectedResponse = [
+      { id: 2, value: "Norris" },
+      { id: 1, value: "Chuck" },
+      false
+    ];
+
+    expect(
+      mapValueResponse(keys, field, values, { emptyValue: false })
+    ).toEqual(expectedResponse);
+  });
+});
+
+describe("#mapArrayResponse", () => {
+  let keys;
+  let field;
+  let values;
 
   beforeEach(() => {
-    mapper = Internal.__get__('mapper')
-
-    keys = [1, 2]
-    field = 'id'
+    keys = [1, 2, 3];
+    field = "externalId";
     values = [
-      { id: 1, value: 'Chuck' },
-      { id: 2, value: 'Norris' },
-    ]
-  })
+      { externalId: 1, value: "Chuck" },
+      { externalId: 2, value: "Norris" },
+      { externalId: 3, value: "Bruce" },
+      { externalId: 3, value: "Lee" },
+      { externalId: null, value: "James Bond" }
+    ];
+  });
+
+  it("aligns the correct value to the key", () => {
+    const expectedResponse = [
+      [{ externalId: 1, value: "Chuck" }],
+      [{ externalId: 2, value: "Norris" }],
+      [{ externalId: 3, value: "Bruce" }, { externalId: 3, value: "Lee" }]
+    ];
+
+    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse);
+  });
+
+  it("works on any key", () => {
+    keys = ["Chuck", "Norris"];
+    field = "value";
+
+    const expectedResponse = [
+      [{ externalId: 1, value: "Chuck" }],
+      [{ externalId: 2, value: "Norris" }]
+    ];
+
+    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse);
+  });
+
+  it("works on nested keys", () => {
+    keys = ["Chuck", "Norris"];
+    field = "value.name";
+    values = [
+      { id: 1, value: { name: "Norris" } },
+      { id: 2, value: { name: "Chuck" } }
+    ];
+
+    const expectedResponse = [
+      [{ id: 2, value: { name: "Chuck" } }],
+      [{ id: 1, value: { name: "Norris" } }]
+    ];
+
+    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse);
+  });
+
+  it("adds [] when a key is not found", () => {
+    keys = [1, 2, 4];
+
+    const expectedResponse = [
+      [{ externalId: 1, value: "Chuck" }],
+      [{ externalId: 2, value: "Norris" }],
+      []
+    ];
+
+    expect(mapArrayResponse(keys, field, values)).toEqual(expectedResponse);
+  });
+});
+
+describe("#mapper", () => {
+  let mapper;
+  let keys;
+  let field;
+  let values;
+
+  beforeEach(() => {
+    mapper = Internal.__get__("mapper");
+
+    keys = [1, 2];
+    field = "id";
+    values = [{ id: 1, value: "Chuck" }, { id: 2, value: "Norris" }];
+  });
 
   // Throwing invalid responses
-  it('throws an error when keys is not an array', () => {
-    const keysOptions = ['bogus', 26, null, undefined, false, {}]
+  it("throws an error when keys is not an array", () => {
+    const keysOptions = ["bogus", 26, null, undefined, false, {}];
 
-    for (let keys of keysOptions) {
+    for (const item of keysOptions) {
+      // eslint-disable-next-line no-loop-func
       expect(() => {
-        mapper('find', keys, field, values)
-      }).toThrowError('Keys must be an array')
+        mapper("find", item, field, values);
+      }).toThrowError("Keys must be an array");
     }
-  })
+  });
 
-  it('throws an error when field is not a string', () => {
-    const fieldOptions = [[], 26, null, undefined, false, {}]
+  it("throws an error when field is not a string", () => {
+    const fieldOptions = [[], 26, null, undefined, false, {}];
 
-    for (let field of fieldOptions) {
+    for (const item of fieldOptions) {
+      // eslint-disable-next-line no-loop-func
       expect(() => {
-        mapper('find', keys, field, values)
-      }).toThrowError('Field must be a string')
+        mapper("find", keys, item, values);
+      }).toThrowError("Field must be a string");
     }
-  })
-})
+  });
+});
